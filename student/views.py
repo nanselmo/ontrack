376488297e81c9b0d41da8d2fe3d75e7413f5966
Nicki Anselmo <nickianselmo@gmail.com>
2016-12-13 10:43:03 -0600

    took out google views

--- student/views.py
+++ student/views.py
@@ -17,24 +17,24 @@ from django.db import connection
 #debug
 #import pdb;
 
-def upload_grade_files(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            uploaded_file = request.FILES['html-file-attribute-name']
-            # Write the file to disk
-            fout = open("on-track/student-data/%s" % uploaded_file.name, 'wb')
-            for chunk in uploaded_file.chunks():
-                fout.write(chunk)
-            fout.close()
-            return HttpResponseRedirect('/grade_report/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'student/uploadGradeFiles.html', {'form': form})
-
-def grade_report(request):
-    template_vars=summarize_data("admin")
-    return render(request, "student/ind-teacher-report.html", template_vars)
+# def upload_grade_files(request):
+#     if request.method == 'POST':
+#         form = UploadFileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             uploaded_file = request.FILES['html-file-attribute-name']
+#             # Write the file to disk
+#             fout = open("on-track/student-data/%s" % uploaded_file.name, 'wb')
+#             for chunk in uploaded_file.chunks():
+#                 fout.write(chunk)
+#             fout.close()
+#             return HttpResponseRedirect('/grade_report/')
+#     else:
+#         form = UploadFileForm()
+#     return render(request, 'student/uploadGradeFiles.html', {'form': form})
+#
+# def grade_report(request):
+#     template_vars=summarize_data("admin")
+#     return render(request, "student/ind-teacher-report.html", template_vars)
 
 def google_chart(request):
 
