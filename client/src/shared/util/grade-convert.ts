d4d3e3d04e1c9e0570cc95c623b2154ae939b85c
Michael Ingram <mpingram@cps.edu>
2017-10-24 14:36:36 -0500

    Bugfix and refactor GradeConvert and NWEAConvert utils
    * GradeConvert
      ** remove stringToScore method to prevent future confusion
      about information preservation when StudentScore is converted
      to string. StudentScores that are numeric grades
      (subjGradeMath, subjGradeRead, etc) are converted to letter
      grades by scoreToString(), and cannot be accurately converted
      back to original numeric grades using stringToScore(),
      although name intuitively suggests otherwise.
    
    * NWEAConvert
      ** handle very low and very high rit scores correctly
        (previously returned 'undefined' for rit scores below
        1st percentile or greater than 99th percentile, now
        returns 1st and 99th percentiles, respectively)

--- client/src/shared/util/grade-convert.ts
+++ client/src/shared/util/grade-convert.ts
@@ -2,7 +2,67 @@ import StudentScores from "shared/types/student-scores";
 import StudentScore from "shared/types/student-score";
 import ScoreType from "shared/enums/score-type";
 
-import {ritToPercentile, percentileToRit, NWEATestType} from "shared/util/nwea-convert";
+import {ritToPercentile, 
+  percentileToRit, 
+  NWEATestType,
+  NWEAConvertErrors} from "shared/util/nwea-convert";
+
+export const GradeConvertErrors = {
+  BadScoreType: new Error("Incorrect ScoreType passed to GradeConvert method"),
+  BadScore: new Error("Bad score passed to GradeConvert method"),
+  BadPercentile: new Error("Bad percentile passed to GradeConvert method")
+};
+
+export const scoreToPercentile = (score: StudentScore, scoreType: ScoreType, gradeLevel: number): number => {
+  switch(scoreType) {
+    case ScoreType.nweaMath:
+      return ritToPercentile(score, NWEATestType.Math, gradeLevel);
+    case ScoreType.nweaRead:
+      return ritToPercentile(score, NWEATestType.Reading, gradeLevel);
+    case ScoreType.subjGradeMath:
+    case ScoreType.subjGradeRead:
+    case ScoreType.subjGradeSci:
+    case ScoreType.subjGradeSocStudies:
+      return numberGradeToPercentile(score);
+    default:
+      throw GradeConvertErrors.BadScoreType;
+  }
+
+};
+
+export const percentileToScore = (percentile: number, scoreType: ScoreType, gradeLevel: number): StudentScore => {
+  if(!(percentile >= 1 && percentile <= 99)) {
+    throw GradeConvertErrors.BadPercentile;
+  }
+  switch(scoreType) {
+    case ScoreType.nweaMath:
+      return percentileToRit(percentile, NWEATestType.Math, gradeLevel);
+    case ScoreType.nweaRead:
+      return percentileToRit(percentile, NWEATestType.Reading, gradeLevel);
+    case ScoreType.subjGradeMath:
+    case ScoreType.subjGradeRead:
+    case ScoreType.subjGradeSci:
+    case ScoreType.subjGradeSocStudies:
+      return gradePercentileToNumberGrade(percentile);
+    default:
+      throw GradeConvertErrors.BadScoreType;
+  }
+};
+
+export const scoreToString = (score: StudentScore, scoreType: ScoreType): string => {
+  switch(scoreType) {
+    case ScoreType.nweaMath:
+    case ScoreType.nweaRead:
+      return score.toString(10);
+    case ScoreType.subjGradeMath:
+    case ScoreType.subjGradeRead:
+    case ScoreType.subjGradeSci:
+    case ScoreType.subjGradeSocStudies:
+      return toLetterGrade(score);
+    default:
+      throw GradeConvertErrors.BadScoreType;
+  }
+};
 
 const letterGradeHighScores = {
   A: 100,
@@ -10,6 +70,52 @@ const letterGradeHighScores = {
   C: 79,
   D: 69,
   F: 59
+};
+
+const isLetterGrade = (strScore: string): boolean => {
+  const letterGrades = ["A", "B", "C", "D", "F"];
+  if (letterGrades.indexOf(strScore.toUpperCase()) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isNumberGrade = (strScore: string): boolean => {
+  const intScore: number = Number.parseInt(strScore, 10);
+  const parseSuccessful = !Number.isNaN(intScore);
+  if (parseSuccessful) {
+    if (intScore >= 0 && intScore <= 100) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
+
+const toNumberGrade = (strScore: string): number => {
+  if (isLetterGrade(strScore)) {
+    return Number.parseInt(strScore);
+  } else if (isNumberGrade(strScore)) {
+    return letterGradeHighScores[strScore];
+  } else {
+    throw new Error(`toNumberGrade parse failed with ${strScore}`);
+  }
+};
+
+
+const toLetterGrade = (grade: number): string => {
+  if (grade <= letterGradeHighScores["F"]) {
+    return "F";
+  } else if (grade <= letterGradeHighScores["D"]) {
+    return "D";
+  } else if (grade <= letterGradeHighScores["C"]) {
+    return "C";
+  } else if (grade <= letterGradeHighScores["B"]) {
+    return "B";
+  } else {
+    return "A";
+  }
 };
 
 const createNumberRangeMapFunction = (inputRange: [number,number], outputRange: [number, number]): (number) => number => {
@@ -26,7 +132,7 @@ const createNumberRangeMapFunction = (inputRange: [number,number], outputRange: 
     }
     // map x onto output range 
     const xMappedToOutput: number = (x - inputLow) / (inputHigh - inputLow) * (outputHigh - outputLow) + outputLow;
-    return xMappedToOutput; 
+    return Math.round(xMappedToOutput); 
   };
 };
 
@@ -35,109 +141,4 @@ const numberGradeRange = [50,100];
 const percentileRange = [1,99];
 const gradePercentileToNumberGrade = createNumberRangeMapFunction([1, 99], [50,100]);
 const numberGradeToPercentile = createNumberRangeMapFunction([50,100], [1,99]);
-
-export const scoreToPercentile = (score: StudentScore, scoreType: ScoreType, gradeLevel: number): number => {
-  switch(scoreType) {
-    case ScoreType.nweaMath:
-      return ritToPercentile(score, NWEATestType.Math, gradeLevel);
-    case ScoreType.nweaRead:
-      return ritToPercentile(score, NWEATestType.Reading, gradeLevel);
-    case ScoreType.subjGradeMath:
-    case ScoreType.subjGradeRead:
-    case ScoreType.subjGradeSci:
-    case ScoreType.subjGradeSocStudies:
-      return numberGradeToPercentile(score);
-  }
-
-};
-
-export const percentileToScore = (percentile: number, scoreType: ScoreType, gradeLevel: number): StudentScore => {
-  switch(scoreType) {
-    case ScoreType.nweaMath:
-      return percentileToRit(percentile, NWEATestType.Math, gradeLevel);
-    case ScoreType.nweaRead:
-      return percentileToRit(percentile, NWEATestType.Reading, gradeLevel);
-    case ScoreType.subjGradeMath:
-    case ScoreType.subjGradeRead:
-    case ScoreType.subjGradeSci:
-    case ScoreType.subjGradeSocStudies:
-      return gradePercentileToNumberGrade(percentile);
-  }
-};
-
-export const scoreToString = (score: StudentScore, scoreType: ScoreType): string => {
-  switch(scoreType) {
-    case ScoreType.nweaMath:
-    case ScoreType.nweaRead:
-      return score.toString(10);
-    case ScoreType.subjGradeMath:
-    case ScoreType.subjGradeRead:
-    case ScoreType.subjGradeSci:
-    case ScoreType.subjGradeSocStudies:
-      return toLetterGrade(score);
-  }
-};
-
-export const stringToScore = (scoreString: string, scoreType: ScoreType): StudentScore => {
-  switch(scoreType) {
-    case ScoreType.nweaMath:
-    case ScoreType.nweaRead:
-      return Number.parseInt(scoreString, 10);
-    case ScoreType.subjGradeMath:
-    case ScoreType.subjGradeRead:
-    case ScoreType.subjGradeSci:
-    case ScoreType.subjGradeSocStudies:
-      return toNumberGrade(scoreString);
-  }
-}
-export const isLetterGrade = (strScore: string): boolean => {
-  const letterGrades = ["A", "B", "C", "D", "F"];
-  if (letterGrades.indexOf(strScore.toUpperCase()) !== -1) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const isNumberGrade = (strScore: string): boolean => {
-  const intScore: number = Number.parseInt(strScore, 10);
-  const parseSuccessful = !Number.isNaN(intScore);
-  if (parseSuccessful) {
-    if (intScore >= 0 && intScore <= 100) {
-      return true;
-    }
-  } else {
-    return false;
-  }
-};
-
-export const toNumberGrade = (strScore: string): number => {
-  if (isLetterGrade(strScore)) {
-    return Number.parseInt(strScore);
-  } else if (isNumberGrade(strScore)) {
-    return letterGradeHighScores[strScore];
-  } else {
-    throw new Error(`toNumberGrade parse failed with ${strScore}`);
-  }
-};
-
-
-export const toLetterGrade = (grade: number): string => {
-  if (grade <= letterGradeHighScores["F"]) {
-    return "F";
-  } else if (grade <= letterGradeHighScores["D"]) {
-    return "D";
-  } else if (grade <= letterGradeHighScores["C"]) {
-    return "C";
-  } else if (grade <= letterGradeHighScores["B"]) {
-    return "B";
-  } else {
-    return "A";
-  }
-};
-
-// FIXME: this is a mock
-export const toGPA = (scores: StudentScores): string => {
-  return "3.0";
-};
 
--- client/src/shared/util/nwea-convert.ts
+++ client/src/shared/util/nwea-convert.ts
@@ -12,9 +12,63 @@ export enum NWEATestType {
 };
 
 export const NWEAConvertErrors = {
+  BadPercentile: new Error("Percentile not in 1-99"),
   BadGradeLevel: new Error("Grade level not in 2-8"),
+  BadRitScore: new Error("Rit score is NaN or negative"),
+  BadTestType: new Error("Unknown test type"),
   TestingSessionNotImplemented: new Error("No records for this testing session."),
   PercentileLookupError: new Error("RIT score matching percentile not found.")
+};
+
+export const percentileToRit = (percentile: number, 
+      testType: NWEATestType, 
+      gradeLevel: number,
+      testSession: NWEATestingSession = NWEATestingSession.Spring): number => {
+
+  if (!(percentile >= 1 && percentile <= 99)) {
+    throw NWEAConvertErrors.BadPercentile;
+  }
+  if (!(gradeLevel >= 2 && gradeLevel <= 8)) {
+    throw NWEAConvertErrors.BadGradeLevel;
+  }
+  if (testType !== NWEATestType.Math && testType !== NWEATestType.Reading) {
+    throw NWEAConvertErrors.BadTestType;
+  }
+  if (testSession !== NWEATestingSession.Spring) {
+    throw NWEAConvertErrors.TestingSessionNotImplemented;
+  }
+  switch(testType) {
+    case NWEATestType.Math: 
+        return findRit(MathPercentileRitLookup[gradeLevel], percentile);
+    case NWEATestType.Reading: 
+        return findRit(ReadPercentileRitLookup[gradeLevel], percentile);
+  }
+};
+
+export const ritToPercentile = (rit: number, 
+      testType: NWEATestType, 
+      gradeLevel: number,
+      testSession: NWEATestingSession = NWEATestingSession.Spring): number => {
+
+  if (!(gradeLevel >= 2 && gradeLevel <= 8)) {
+    throw NWEAConvertErrors.BadGradeLevel;
+  }
+  if (testType !== NWEATestType.Math && testType !== NWEATestType.Reading) {
+    throw NWEAConvertErrors.BadTestType;
+  }
+  if (testSession !== NWEATestingSession.Spring) {
+    throw NWEAConvertErrors.TestingSessionNotImplemented;
+  }
+  if (!(rit > 0)) {
+    throw NWEAConvertErrors.BadRitScore;
+  }
+
+  switch(testType) {
+    case NWEATestType.Math: 
+      return findPercentile(MathPercentileRitLookup[gradeLevel], rit);
+    case NWEATestType.Reading: 
+      return findPercentile(ReadPercentileRitLookup[gradeLevel], rit);
+  }
 };
 
 const findPercentile = (ritByPercentile: number[], targetRit: number): number => {
@@ -24,11 +78,17 @@ const findPercentile = (ritByPercentile: number[], targetRit: number): number =>
     const currRit = ritByPercentile[i];
     const nextRit = ritByPercentile[i + 1];
     const percentile = i + 1;
+    // on first iteration (percentile 1),
+    // return current percentile if rit is less than nextRit
+    if (i === 0) {
+      if (targetRit < nextRit) {
+        return percentile;
+      }
     // on last iteration, nextRit is undefined
     // and percentile = 99. So, if targetRit is greater than
     // currRit, targetRit is in 99th percentile.
-    if (i === ritByPercentile.length - 1) {
-      if (targetRit < nextRit) {
+    } else if (i === ritByPercentile.length - 1) {
+      if (targetRit >= currRit) {
         return percentile;
       }
     // on all other iterations (percentiles 1 - 98),
@@ -43,112 +103,11 @@ const findPercentile = (ritByPercentile: number[], targetRit: number): number =>
 };
 
 const findRit = (percentileArray: number[], percentile: number): number => {
-  const rit = percentileArray[percentile];
+  const index = percentile - 1;
+  const rit = percentileArray[index];
   if (rit === undefined) {
     throw NWEAConvertErrors.PercentileLookupError;
   } else {
     return rit;
   }
 };
-
-
-export const percentileToRit = (percentile: number, 
-      testType: NWEATestType, 
-      gradeLevel: number,
-      testSession: NWEATestingSession = NWEATestingSession.Spring): number => {
-
-  if (gradeLevel < 2 || gradeLevel > 8) {
-    throw NWEAConvertErrors.BadGradeLevel;
-  }
-  if (testSession !== NWEATestingSession.Spring) {
-    throw NWEAConvertErrors.TestingSessionNotImplemented;
-  }
-  switch(testType) {
-    case NWEATestType.Math: 
-      switch(gradeLevel) {
-        case 2:
-          return findRit(MathPercentileRitLookup["Grade_2"], percentile);
-        case 3:
-          return findRit(MathPercentileRitLookup["Grade_3"], percentile); 
-        case 4:
-          return findRit(MathPercentileRitLookup["Grade_4"], percentile); 
-        case 5:
-          return findRit(MathPercentileRitLookup["Grade_5"], percentile); 
-        case 6:
-          return findRit(MathPercentileRitLookup["Grade_6"], percentile); 
-        case 7:
-          return findRit(MathPercentileRitLookup["Grade_7"], percentile); 
-        case 8:
-          return findRit(MathPercentileRitLookup["Grade_8"], percentile); 
-      }
-    case NWEATestType.Reading: 
-      switch(gradeLevel) {
-        case 2:
-          return findRit(ReadPercentileRitLookup["Grade_2"], percentile);
-        case 3:
-          return findRit(ReadPercentileRitLookup["Grade_3"], percentile); 
-        case 4:
-          return findRit(ReadPercentileRitLookup["Grade_4"], percentile); 
-        case 5:
-          return findRit(ReadPercentileRitLookup["Grade_5"], percentile); 
-        case 6:
-          return findRit(ReadPercentileRitLookup["Grade_6"], percentile); 
-        case 7:
-          return findRit(ReadPercentileRitLookup["Grade_7"], percentile); 
-        case 8:
-          return findRit(ReadPercentileRitLookup["Grade_8"], percentile); 
-      }
-  }
-};
-
-export const ritToPercentile = (rit: number, 
-      testType: NWEATestType, 
-      gradeLevel: number,
-      testSession: NWEATestingSession = NWEATestingSession.Spring): number => {
-
-  if (gradeLevel < 2 || gradeLevel > 8) {
-    throw NWEAConvertErrors.BadGradeLevel;
-  }
-  if (testSession !== NWEATestingSession.Spring) {
-    throw NWEAConvertErrors.TestingSessionNotImplemented;
-  }
-
-  switch(testType) {
-    case NWEATestType.Math: 
-      switch(gradeLevel) {
-        case 2:
-          return findPercentile(MathPercentileRitLookup["Grade_2"], rit);
-        case 3:
-          return findPercentile(MathPercentileRitLookup["Grade_3"], rit); 
-        case 4:
-          return findPercentile(MathPercentileRitLookup["Grade_4"], rit); 
-        case 5:
-          return findPercentile(MathPercentileRitLookup["Grade_5"], rit); 
-        case 6:
-          return findPercentile(MathPercentileRitLookup["Grade_6"], rit); 
-        case 7:
-          return findPercentile(MathPercentileRitLookup["Grade_7"], rit); 
-        case 8:
-          return findPercentile(MathPercentileRitLookup["Grade_8"], rit); 
-      }
-    case NWEATestType.Reading: 
-      switch(gradeLevel) {
-        case 2:
-          return findPercentile(ReadPercentileRitLookup["Grade_2"], rit);
-        case 3:
-          return findPercentile(ReadPercentileRitLookup["Grade_3"], rit); 
-        case 4:
-          return findPercentile(ReadPercentileRitLookup["Grade_4"], rit); 
-        case 5:
-          return findPercentile(ReadPercentileRitLookup["Grade_5"], rit); 
-        case 6:
-          return findPercentile(ReadPercentileRitLookup["Grade_6"], rit); 
-        case 7:
-          return findPercentile(ReadPercentileRitLookup["Grade_7"], rit); 
-        case 8:
-          return findPercentile(ReadPercentileRitLookup["Grade_8"], rit); 
-      }
-  }
-};
-
-
