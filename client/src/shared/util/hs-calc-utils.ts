394ec561d461afe05ec240ed3bb7ca7a5de9b04d
Michael Ingram <mpingram@cps.edu>
2017-11-07 13:11:37 -0600

    Minor refactor shared/util/hs-calc-utils

--- client/src/shared/util/hs-calc-utils.ts
+++ client/src/shared/util/hs-calc-utils.ts
@@ -4,16 +4,15 @@ import {ritToPercentile, NWEATestType} from "shared/util/nwea-convert";
 
 export const calculateSEPoints = (data: StudentData, additionalReqs: {SETestPercentile: number}): number => {
 
-  let sePoints = 0;
-  // calculate score component for NWEA scores
+  // calculate points for NWEA scores
   const NWEA_SCORE_CONSTANT = 1.515;
   const mathPercentile = ritToPercentile(data.scores.nweaMath, NWEATestType.Math, data.gradeLevel);
   const readPercentile = ritToPercentile(data.scores.nweaRead, NWEATestType.Reading, data.gradeLevel);
 
-  sePoints += mathPercentile * NWEA_SCORE_CONSTANT;
-  sePoints += readPercentile * NWEA_SCORE_CONSTANT;
+  const nweaMathPoints = Math.round(mathPercentile * NWEA_SCORE_CONSTANT);
+  const nweaReadPoints = Math.round(readPercentile * NWEA_SCORE_CONSTANT);
 
-  // calculate score component for subjGrades
+  // calculate points for subjGrades
   const gradePointsLookup = {
     "A": 75,
     "B": 50,
@@ -21,33 +20,39 @@ export const calculateSEPoints = (data: StudentData, additionalReqs: {SETestPerc
     "D": 0,
     "F": 0,
   }
-  sePoints += gradePointsLookup[toLetterGrade(data.scores.subjGradeMath)];
-  sePoints += gradePointsLookup[toLetterGrade(data.scores.subjGradeRead)];
-  sePoints += gradePointsLookup[toLetterGrade(data.scores.subjGradeSci)];
-  sePoints += gradePointsLookup[toLetterGrade(data.scores.subjGradeSocStudies)];
+  const subjGradeMathPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeMath)];
+  const subjGradeReadPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeRead)];
+  const subjGradeSciPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeSci)];
+  const subjGradeSocStudiesPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeSocStudies)];
 
   // calculate score component for SE Test percentile 
   const SE_TEST_PERCENTILE_CONSTANT = 3.03; 
-  // FIXME: working around absence of additionalReqs right now
-  if (additionalReqs.SETestPercentile) {
-    sePoints += additionalReqs.SETestPercentile * SE_TEST_PERCENTILE_CONSTANT;
-    console.warn("No SETestPercentile passed to calculateSEPoints! Matching average of NWEA Math/Read percentiles instead.");
-  } else {
-    sePoints += ( mathPercentile + readPercentile / 2 ) * SE_TEST_PERCENTILE_CONSTANT;
+  // throw if no additionalReqs passed
+  if (!additionalReqs.SETestPercentile) {
+    throw new Error("No SETestPercentile passed to calculateSEPoints!");
   }
-  console.log(sePoints);
+  const seTestPoints = Math.round(additionalReqs.SETestPercentile * SE_TEST_PERCENTILE_CONSTANT);
+  
+  const sePoints = nweaMathPoints +
+    nweaReadPoints +
+    subjGradeMathPoints +
+    subjGradeReadPoints + 
+    subjGradeSciPoints +
+    subjGradeSocStudiesPoints +
+    seTestPoints;
+
   return sePoints;
 };
 
-export const calculateIBPoints = (data: StudentData, additionalReqs: {attendedInfoSession: boolean}): number => {
-  let ibPoints = 0;
-  // calculate score component for NWEA scores 
+export const calculateIBPoints = (data: StudentData): number => {
+  // calculate points for NWEA scores
   const NWEA_SCORE_CONSTANT = 2.2727;
   const mathPercentile = ritToPercentile(data.scores.nweaMath, NWEATestType.Math, data.gradeLevel);
   const readPercentile = ritToPercentile(data.scores.nweaRead, NWEATestType.Reading, data.gradeLevel);
-  ibPoints += mathPercentile * NWEA_SCORE_CONSTANT;
-  ibPoints += readPercentile * NWEA_SCORE_CONSTANT;
-  
+
+  const nweaMathPoints = Math.round(mathPercentile * NWEA_SCORE_CONSTANT);
+  const nweaReadPoints = Math.round(readPercentile * NWEA_SCORE_CONSTANT);
+
   // calculate score component for subj grades
   const gradePointsLookup = {
     "A": 112.5,
@@ -56,14 +61,20 @@ export const calculateIBPoints = (data: StudentData, additionalReqs: {attendedIn
     "D": 0,
     "F": 0,
   }
-  ibPoints += gradePointsLookup[data.scores.subjGradeMath];
-  ibPoints += gradePointsLookup[data.scores.subjGradeRead];
-  ibPoints += gradePointsLookup[data.scores.subjGradeSci];
-  ibPoints += gradePointsLookup[data.scores.subjGradeSocStudies];
+  const subjGradeMathPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeMath)];
+  const subjGradeReadPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeRead)];
+  const subjGradeSciPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeSci)];
+  const subjGradeSocStudiesPoints = gradePointsLookup[toLetterGrade(data.scores.subjGradeSocStudies)];
   
   // is student within attendance boundaries?
   // TODO: this is hard to do correctly. Can just compute flat-earth distance as a stopgap
   // until can architect a solution involving looking up coordinates in geoJSON and stuff
+  const ibPoints = nweaMathPoints +
+    nweaReadPoints +
+    subjGradeMathPoints +
+    subjGradeReadPoints + 
+    subjGradeSciPoints +
+    subjGradeSocStudiesPoints;
 
   return ibPoints;
 };
